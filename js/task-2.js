211e8f7933b78f36ec2195a9a1ccb59862a0c728
SwitcheR <19fkf86@gmail.com>
2026-08-30 16:29:43 +0300

    UPD task-2 / completed

--- js/task-2.js
+++ js/task-2.js
@@ -2,9 +2,8 @@ function formatMessage (message, maxLength) {
     if (message.length <= maxLength) {
         return message;
     } else {
-        message.slice(0, maxLength);
+        return message.slice(0, maxLength) + "...";
     }
-    return message.slice(0, maxLength) + "...";
 }
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
